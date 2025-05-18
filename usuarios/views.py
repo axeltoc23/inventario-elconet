4ddf746705dfe3b8b77a9eccd706187d31750b86
axeltoc23 <3006657680101@ingenieria.usac.edu.gt>
2025-05-18 11:40:07 -0600

    cambios para que el login cuando no ingrese solo borre contraseña, falta que también muestre que la contraeseña está mala

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,30 +1,31 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.contrib.auth.views import LoginView
 
 
 def user_login(request):
+    form = AuthenticationForm(request, data=request.POST or None)
+
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
-            return redirect("lista_productos")  # Redirigir a la lista de productos después de login
+            return redirect("lista_productos")
         else:
             messages.error(request, "Usuario o contraseña incorrectos.")
 
-    return render(request, "usuarios/login.html")
+    return render(request, "usuarios/login.html", {"form": form})
 
 
 def user_logout(request):
     logout(request)
-    return redirect("user_login")  # Redirigir al login después de cerrar sesión
+    return redirect("user_login")
 
 
 class CustomLoginView(LoginView):
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect("/productos/")  # O redirigir a "/productos/"
+            return redirect("/productos/")
         return super().get(request, *args, **kwargs)
